a564b43f947973ba5f5ad8785e06bc8308842bbb
Daniel RG <21danielrondong@gmail.com>
2023-01-29 19:04:19 +0100

    market model set properties

---
+++ models/Market.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+ 
+const marketSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a market name']
+    },
+    logo: {
+      type: String,
+      default: './images/market default edit.png'
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+const Market = model('Market', marketSchema);
+
+module.exports = Market;
