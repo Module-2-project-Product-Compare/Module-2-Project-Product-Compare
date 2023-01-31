c717f0725c3e99d747c1cae04461f070f64a02d4
Giovanni Bedoya <giovedoya@gmail.com>
2023-01-31 17:42:48 +0100

    router edit and search in progress

--- routes/products.js
+++ routes/products.js
@@ -14,6 +14,9 @@ router.get('/', async function (req, res, next) {
     }
 });
 
+// @desc    Admin can create new products in the database
+// @route   POST /products
+// @access  Privat
 router.get('/new', function (req, res, next){
     res.render('newProduct');
   })
@@ -39,6 +42,33 @@ router.get('/search', async function (req, res, next) {
   try {
     const product = await Product.findOne({ name: name });
     res.render('search', { query: name, product: product });
+  } catch (error) {
+    next(error)
+  }
+});
+
+// @desc    Edit form products in the database
+// @route   POST /edit/:productsId
+// @access  Privat
+router.get('/edit/:productId', async function (req, res, next) {
+  const { productId } = req.params;
+  try {
+    const product = await Product.findById(productId);
+    res.render('editProduct', product);
+  } catch (error) {
+    next(error)
+  }
+});
+
+// @desc    Admin can edit products in the database
+// @route   POST /edit/:productsId
+// @access  Privat
+router.post('/edit/:productId', async function (req, res, next) {
+  const { name, format, image, price  } = req.body;
+  const { productId } = req.params;
+  try {
+    const editedProduct = await Product.findByIdAndUpdate(productId, { name, format, image, price }, { new: true });
+    res.redirect(`/products/${editedProduct._id}`);
   } catch (error) {
     next(error)
   }
