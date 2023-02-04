e5e48f7c201c882dd07f43a4b149df1003f61744
Daniel-RG <21danielrondong@gmail.com>
2023-02-04 12:25:08 +0100

    Changes in market database

--- data/marketsData.js
+++ data/marketsData.js
@@ -1,95 +1,22 @@
 module.exports = [
-//CP 08003:
   {
     name: 'Alcampo',
-    logo: '../images/logo-alcampo.png',
-    postalCode: 08003,
-    address: 'Via Laietana, 4, Barcelona'
+    logo: '../images/logo-alcampo.png'
   },
   {
     name: 'Bonarea',
-    logo: '../images/logo-bonarea.png',
-    postalCode: 08003,
-    address: 'C. de Sant Pere Més Baix, 30, Barcelona'
+    logo: '../images/logo-bonarea.png'
   },
   {
     name: 'Carrefour',
-    logo: '../images/logo-carrefour.png',
-    postalCode: 08003,
-    address: 'Via Laietana, 48A, Barcelona'
+    logo: '../images/logo-carrefour.png'
   },
   {
     name: 'Dia',
-    logo: '../images/logo-dia.png',
-    postalCode: 08003,
-    address: 'Carrer de Alí Bei, 3, Barcelona'
+    logo: '../images/logo-dia.png'
   },
   {
     name: 'Mercadona',
-    logo: '../images/logo-mercadona.png',
-    postalCode: 08003,
-    address: 'Rda. de Sant Pere, 31, Barcelona'
+    logo: '../images/logo-mercadona.png'
   },
-//CP 08005:
-  {
-    name: 'Alcampo',
-    logo: '../images/logo-alcampo.png',
-    postalCode: 08005,
-    address: 'Plaça Diagonal Mar, 15, Barcelona'
-  },
-  {
-    name: 'Bonarea',
-    logo: '../images/logo-bonarea.png',
-    postalCode: 08005,
-    address: 'C. de Ramon Turró, 220, Barcelona'
-  },
-  {
-    name: 'Carrefour',
-    logo: '../images/logo-carrefour.png',
-    postalCode: 08005,
-    address: 'C. Les Glories, esquina Calle Llacunna, 155, Barcelona'
-  },
-  {
-    name: 'Dia',
-    logo: '../images/logo-dia.png',
-    postalCode: 08005,
-    address: 'Rambla del Poblenou, 55, Barcelona'
-  },
-  {
-    name: 'Mercadona',
-    logo: '../images/logo-mercadona.png',
-    postalCode: 08005,
-    address: 'C. de Pallars, 221, Barcelona'
-  },
-  //CP 08036:
-  {
-    name: 'Alcampo',
-    logo: '../images/logo-alcampo.png',
-    postalCode: 08036,
-    address: 'C. de Enric Granados, 38, Barcelona'
-  },
-  {
-    name: 'Bonarea',
-    logo: '../images/logo-bonarea.png',
-    postalCode: 08036,
-    address: 'C. de Villarroel, 137, Barcelona'
-  },
-  {
-    name: 'Carrefour',
-    logo: '../images/logo-carrefour.png',
-    postalCode: 08036,
-    address: 'C. de Berlín, 50, 54, Barcelona'
-  },
-  {
-    name: 'Dia',
-    logo: '../images/logo-dia.png',
-    postalCode: 08036,
-    address: 'C. de París, 120, Barcelona'
-  },
-  {
-    name: 'Mercadona',
-    logo: '../images/logo-mercadona.png',
-    postalCode: 08036,
-    address: 'C. de Mallorca, 135, Barcelona'
-  }
 ]
