cf9d8da0fdc4f8a6a9f808009570967f9ee03a9d
Daniel-RG <21danielrondong@gmail.com>
2023-02-04 14:52:16 +0100

    commit before merge models-ids branch

--- data/products.js
+++ data/products.js
@@ -1,25 +1,30 @@
 module.exports = [
     {
+        _id: ObjectId('63de3e43869f79fd4dc59de8'),
         name: 'leche Asturiana',
         format: 'Tetra brick',
         category: "leche"
     },
     {
+        _id: ObjectId('63de3e43869f79fd4dc59de9'),
         name: 'Arroz SOS',
         format: '500g',
         category: "arroz"
     },
     {
+        _id: ObjectId('63de3e43869f79fd4dc59dea'),
         name: 'Espagueti Gallo',
         format: '500g',
         category: "espagueti"
     },
     {
+        _id: ObjectId('63de3e43869f79fd4dc59deb'),
         name: 'Huevos El Corral',
         format: '12 unidades',
         category: "huevos"
     },
     {
+        _id: ObjectId('63de3e43869f79fd4dc59dec'),
         name: 'Pan Bimbo',
         format: 'Bimbo grande',
         category: "pan"     
