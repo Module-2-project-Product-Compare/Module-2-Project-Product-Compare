faaa275bab96cffe8fcaab4f6ba5a9718ac2dd48
Giovanni Bedoya <giovedoya@gmail.com>
2023-02-10 20:16:22 +0100

    styles, routes and views articles

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const productSchema = new Schema(
   {
     category: {
       type: String,
-      enum: [ 'leche', 'huevos', 'pan', 'arroz', 'espagueti']
+      enum: [ 'leche', 'huevos', 'pan', 'arroz', 'espagueti', 'pizza', 'bebida']
     },
     name: {
       type: String,
