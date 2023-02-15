fc4ad55aa8f0d3e89896c08bb104e55afc239b41
Daniel-RG <21danielrondong@gmail.com>
2023-02-15 23:38:13 +0100

    styling views

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,19 @@ const Product = require('../models/Product');
 // @access  Public
 router.get('/', async function (req, res, next) {
     const user = req.session.currentUser;
+    let showAdminBtns = undefined;
+    if (user){
+        const userRole = user.role;
+    if (userRole === "admin") {
+        showAdminBtns = true;
+    } else {
+        showAdminBtns = false;
+    }};
     try {        
         const products = await Product.find({});
         const articles = await Article.find({});
         const uniqueCategories = [...new Set(products.map(product => product.category))];
-        res.render('index', { products, articles, uniqueCategories, user });
+        res.render('index', { products, articles, uniqueCategories, user, showAdminBtns });
     } catch (error) {
         next(error)
     }
